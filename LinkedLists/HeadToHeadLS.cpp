5789754c1112e46873e3d3e582dae820f5ec7fe2
math_pi <95710218+snmath-pi@users.noreply.github.com>
2022-05-04 18:52:07 +0530

    Create HeadToHeadLS.cpp

---
+++ LinkedLists/HeadToHeadLS.cpp
@@ -0,0 +1,103 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Node{
+	int data ;
+struct Node*next ;
+} *first=NULL ;
+void create(int a[] , int n){
+	int i ;
+	struct Node*t , *last ;
+	first = new Node() ;
+	first->data = a[0] ;
+	first->next = NULL ;
+	last = first ;
+	for(int i=1;i<n;++i){
+		t= new Node() ;
+		t->data = a[i] ;
+		t->next = NULL ;
+		last->next = t ;
+		last = t ;
+
+	}
+	
+}
+void display(struct Node*p){
+	while(p){
+		printf("%d " , p->data) ;
+		p = p->next ;
+
+	}
+	cout<<"\n" ;
+
+}
+//USING RECURSION FOR PRINTING THE ELEMENTS OF THE LINKED LIST 
+void RDisplay(struct Node*p){
+	if(p!=NULL){
+		printf("%d ", p->data) ;
+		RDisplay(p->next) ;
+		
+	}
+
+}
+//USING RECURSION FOR PRINTING ELEMENTS IN REVERSED OREDER
+void RRDisplay(struct Node*p){
+	if(p!=NULL){
+		RRDisplay(p->next) ;
+		printf("%d ",p->data) ;
+	}
+}
+int Count(struct Node*p){
+	int cnt(0) ;
+	while(p){
+		p=p->next; 
+		cnt++ ;
+	}
+	cout<< cnt ;
+}
+int CountR(struct Node*p){
+	if(!p) return 0 ;
+	else {
+		return CountR(p->next)+1 ;
+	}
+}
+int Sum(struct Node*p){
+	int sum(0) ;
+	while(p){
+		sum+=p->data ;
+		p = p->next ;
+	}
+	return sum ;
+}
+// TO PRINT MAXIMUM ELEMENT
+int MAX(struct Node*p){
+	int maxx = INT_MIN ;
+	while(p){
+		if(p->data>maxx) maxx = p->data ;
+		p=p->next ;
+	}
+	return maxx ;
+}
+struct Node * Lsearch(struct Node*p , int key){
+	struct Node*q =NULL ;
+	while(p){
+		if(key==p->data){
+			q->next = p->next ;
+			p->next = first ;
+			first = p ;
+			return p ;
+		}
+		q = p ;
+		p=p->next ;
+
+	}
+	return NULL ;
+
+}
+int main(){
+	int a[] = {1,2,3,4,5} ;
+	int n = sizeof(a)/sizeof(a[0]) ;
+	create(a,n) ;
+	cout<<Lsearch(first,3)->data	;
+	cout<<"\n" ;
+
+}
