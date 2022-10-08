df05c7cc8a024f9760d41fa559eb397b973dda5d
math_pi <95710218+snmath-pi@users.noreply.github.com>
2022-10-08 13:12:09 +0530

    Update TotalNoOfNodesInCompleteTree.cpp
    Self Notes:
    🍇 Formula is (2^TreeLevel - 1). Only works for perfect tree.
    🍇 To determine if its a perfect tree, go all the way down and count the nodes on left and right side, If they match, its a perfect tree and our formula applies.
    🍇 If its not a perfect tree, we go on left and right subtree and again determine if they are perfect tree.
    🍇 When we have our left and right heights, we do (1 + left + right)
    🍇 If we reach null, return 0
    🍇 C++ note: 1 << n is the same as raising 2 to the power n, or 2^n

--- Trees/TotalNoOfNodesInCompleteTree.cpp
+++ Trees/TotalNoOfNodesInCompleteTree.cpp
@@ -1,1 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std ;
+class Node{
+public:
+	int data ;
+	Node* left ;
+	Node* right ;
+	Node(int val){
+		this->data = val ;
+		left = right = NULL ;
+	}
+};
+int leftheight(Node* root){
+	int h = 0 ;
+	while(root){
+		h++ ;
+		root = root->left ;
+	}
+	return h ;
+}
+int rightheight(Node* root){
+	int h = 0 ;
+	while(root){
+		h++ ;
+		root = root->right ;
+	}
+	return h;
+}
+int CountNodesOfCompleteTree(Node * root){
+	if(!root) return 0 ;
+	int lh = leftheight(root);
+	int rh = rightheight(root) ;
+	if(lh == rh) return (1<<lh ) - 1 ;
+	return 1 + CountNodesOfCompleteTree(root->left) + CountNodesOfCompleteTree(root->right) ;
+}
+int main(){
+	Node* root = new Node(1) ;
+	root->left = new Node(2) ;
+	root->right = new Node(3) ;
+	root->left->left= new Node(4) ;
+	root->left->right = new Node(5) ;
+	root->right->left = new Node(6) ;
+	
+	cout << CountNodesOfCompleteTree(root ) ;
 
+
+}
