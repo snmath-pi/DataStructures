fec5e35e11c8fc8a1414390a4470656e6c68898c
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2023-06-17 13:12:37 +0530

    Create 35.cpp

---
+++ 450DSASHEET/Array/35.cpp
@@ -0,0 +1,168 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <climits>
+#include <cmath>
+#include <complex>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <vector>
+#define rep(i,n) for(int i=0;i<n;++i)
+#define per(i,n) for(int i=n-1;i>=0;--i)
+#define repn(i,n) for(int i=1;i<=n;++i)
+#define rng(i, a, b) for(int i=a; i<b; i++)
+#define gnr(i,a,b) for(int i=b-1;i>=a;--i)
+#define vb vector<bool>
+#define vi vector<int>
+#define vl vector<ll>
+#define vvb vector<vb>
+#define vvi vector<vi>
+#define vvl vector<vl>
+#define si unordered_set<int>
+#define sl unordered_set<ll>
+#define tsi set<int>
+#define tsl set<ll>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vpii vector<pii>
+#define vpll vector<pll>
+#define tmii map<int, int>
+#define tmll map<ll, ll>
+#define mii unordered_map<int, int>
+#define mll unordered_map<ll, ll>
+#define pqi priority_queue<int>
+#define pqig priority_queue<int, vi, greater<int>>
+#define pql priority_queue<ll>
+#define pqlg priority_queue<ll, vl, greater<ll>>
+#define pqpii priority_queue<pii>
+#define pqpll priority_queue<pll>
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int) (x).size()
+#define ll long long
+#define ld long double
+#define nl '\n'
+#define sp ' '
+#define fi first
+#define se second
+#define pb push_back
+#define mpr make_pair
+ 
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << sp; use_(x); cerr << nl;
+#else
+#define debug(x)
+#endif
+
+template<typename T> bool chkmin(T &a, T b){return (b < a) ? a = b, 1 : 0;}
+template<typename T> bool chkmax(T &a, T b){return (b > a) ? a = b, 1 : 0;}
+
+int dx4[4]={1,-1,0,0};
+int dy4[4]={0,0,1,-1};
+int dx8[8]={0,1,0,-1,1,1,-1,-1};
+int dy8[8]={1,0,-1,0,1,-1,1,-1};
+
+void yesnoc(bool a) {
+    cout << (a? "YES\n": "NO\n");
+}
+void yesnos(bool a) {
+    cout << (a? "Yes\n": "No\n");
+}
+
+void use_(ll W) {cerr << W;}
+void use_(ld W) {cerr << W;}
+void use_(char W) {cerr << W;}
+void use_(string W) {cerr << W;}
+void use_(int W) {cerr << W;}
+void use_(double W) {cerr << W;}
+
+template<typename T, typename V> void use_(pair<T, V> p) {
+    cerr << "["; use_(p.fi); cerr << ','; use_(p.se); cerr << ']';
+}
+template<typename T> void use_(vector<T> a) {
+    cerr << '['; for(T i: a) { use_(i); cerr << sp;
+    } cerr << ']'; cerr << nl;
+}
+template<typename T> void use_(set<T> a) {
+    cerr << '['; for(T i: a) { use_(i); cerr << sp;
+    } cerr << ']'; cerr << nl;
+}
+template<typename T> void use_(multiset<T> ms) {
+    cerr << '['; for(T i: ms) { use_(i); cerr << sp;
+    } cerr << ']'; cerr << nl;
+}
+template<typename T, typename V> void use_(map<T, V> mp) {
+    cerr << '[';  for(auto x: mp) { use_(x.fi); cerr << ','; 
+        use_(x.se); cerr << sp;
+    } cerr << ']'; cerr << nl;
+}
+ 
+int t, n, k;
+ld median(vi &a, vi&b) {
+    int n = sz(a), m = sz(b);
+    if(n > m) {
+        return median(b, a);
+    }
+    int start = 0, end = n, ok = (sz(a) + sz(b)) % 2;
+    int tot = (n + m + 1) >> 1; // ceil value
+    while(start <= end) {
+        int mid = start + end >> 1;
+        int la = mid, lb = tot - mid;
+        int w = la ? a[la-1] : -1e9, x =  lb ? b[lb-1] : -1e9;
+        int y = la < n ? a[la] : 1e9, z = lb < m ? b[lb] : 1e9;
+        debug(mpr(w, x)); debug(mpr(y,z));
+        if(w <= z && y >= x) {
+            if(ok) {
+                debug(max(w,x));
+                return max(w, x);
+            } else {
+                debug(max(w, x)); debug(min(y, z));
+                return (max(w, x) + min(y, z)) / 2.0;
+            }
+        } else if(w > z) {
+            end = mid - 1;
+        } else start = mid + 1;
+    }
+    return 0.0;
+}
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("error.txt", "w", stderr);
+#endif
+    ios::sync_with_stdio(0);
+    cin.tie(0); int m;
+    cin >> n >> m;
+    vi a(n), b(m);
+    rep(i, n) cin >> a[i]; rep(i, m) cin >> b[i];
+    int i = 0, j = 0;
+    /* O(N + M) */
+    vi c;
+    while(i < n && j < m) {
+        if(a[i] <= b[j]) {
+            c.pb(a[i++]);
+        } else c.pb(b[j++]);
+    }
+    while(j < m) c.pb(b[j++]);
+    while(i < n) c.pb(a[i++]);
+    debug(c);
+    if(sz(c) % 2) cout << c[sz(c) / 2] << nl;
+    else cout << (ld) (c[sz(c) / 2] + c[sz(c) / 2 - 1] ) / 2.0 << nl;
+
+
+    /*O(MIN(LOG(M), LOG(N)))*/
+
+    cout << median(a, b) << nl;
+
+
+}
