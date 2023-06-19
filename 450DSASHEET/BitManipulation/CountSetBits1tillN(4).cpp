352958fe267867efe6b77348a44c7c6aedf4a6d1
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2023-06-20 00:04:59 +0530

    Create CountSetBits1tillN(4).cpp

---
+++ 450DSASHEET/BitManipulation/CountSetBits1tillN(4).cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    // n: input to count the number of set bits
+    //Function to return sum of count of set bits in the integers from 1 to n.
+    int countSetBits(int n)
+    {
+        // Your logic here
+        n++;
+        int a = 1;
+        long long cnt = n / 2;
+        while((1<<a) <= n) {
+            cnt += ((n / (1<<a)) / 2) * (1 << a);
+            cnt += ((n / (1 << a)) % 2) ? n % (1 << a) : 0;
+            a++;
+        }
+        return cnt;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+// Driver code
+int main()
+{
+	 int t;
+	 cin>>t;// input testcases
+	 while(t--) //while testcases exist
+	 {
+	       int n;
+	       cin>>n; //input n
+	       Solution ob;
+	       cout << ob.countSetBits(n) << endl;// print the answer
+	  }
+	  return 0;
+}
+
+// } Driver Code Ends
