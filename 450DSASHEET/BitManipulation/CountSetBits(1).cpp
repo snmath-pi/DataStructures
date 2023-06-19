5f751a64e8b5d8c582cee96f9f23b43a161e64ef
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2023-06-19 20:39:54 +0530

    Create CountSetBits(1).cpp

---
+++ 450DSASHEET/BitManipulation/CountSetBits(1).cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int setBits(int N) {
+        // Write Your Code here 
+        int cnt = 0;
+        for(int i=0; i < 31; i++) {
+            cnt += (((1<<i) & N)!=0);
+        }
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+
+        Solution ob;
+        int cnt = ob.setBits(N);
+        cout << cnt << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
