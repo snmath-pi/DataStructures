acadb25db129b255783b0ef0d195abdb5b57ed93
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2023-06-26 10:38:53 +0530

    Create NextGreaterNumber.cpp

---
+++ 450DSASHEET/Strings/NextGreaterNumber.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    vector<int> nextPermutation(int n, vector<int> arr){
+        // code here
+        int i = n - 2;
+        while(i >= 0 && arr[i+1] <= arr[i]) i--;
+        if(i >= 0) {
+            int j = n-1;
+            while(arr[j] <= arr[i]) j--;
+            swap(arr[i], arr[j]);
+        }
+        reverse(arr.begin() + i + 1, arr.end());
+        return arr;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        vector<int> arr(N);
+        for(int i = 0;i < N;i++)
+            cin>>arr[i];
+        
+        Solution ob;
+        vector<int> ans = ob.nextPermutation(N, arr);
+        for(int u: ans)
+            cout<<u<<" ";
+        cout<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
