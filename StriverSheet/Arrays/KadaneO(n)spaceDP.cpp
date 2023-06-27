88e6ff481107f740e184759e4216b11cdbd90719
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2023-06-27 10:09:09 +0530

    Update KadaneO(n)spaceDP.cpp

--- StriverSheet/Arrays/KadaneO(n)spaceDP.cpp
+++ StriverSheet/Arrays/KadaneO(n)spaceDP.cpp
@@ -3,11 +3,13 @@ public:
     int maxSubArray(vector<int>& nums) {
         int n = nums.size();
         int dp[n];
-        memset(dp, -10000000, sizeof(dp));
-        dp[0] = nums[0];
+        memset(dp, -10000000, sizeof(dp)); // set min dp
+        dp[0] = nums[0]; // base case just set it to first element
         for(int i=1; i<n; i++) {
-            dp[i] = max(nums[i], dp[i-1] + nums[i]);
+            dp[i] = max(nums[i], dp[i-1] + nums[i]); // two case now 
+            // dp[i] = nums[i], we decide to freshly start the new 'subarray sum'
+            // dp[i] = dp[i-1] + nums[i], we decide to continue extending current 'subarray sum';
         }
-        return *max_element(dp, dp + n);
+        return *max_element(dp, dp + n); // return the max value in dp
     }
 };
