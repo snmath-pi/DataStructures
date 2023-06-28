0050af86bc2af185cdcdc81e68a438431900250d
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2023-06-28 11:05:14 +0530

    Create RotateMatrix.cpp

---
+++ StriverSheet/Arrays(II)/RotateMatrix.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size(), m = matrix[0].size();
+        for(int i=0; i<n-1; i++) for(int j=i+1; j<m; j++) swap(matrix[i][j], matrix[j][i]);
+        for(int i=0; i<n; i++) reverse(matrix[i].begin(), matrix[i].end());
+    }
+};
