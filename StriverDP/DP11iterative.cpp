f0af80838231e865b35e75faee9c6f58012581e7
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2023-06-29 18:38:39 +0530

    Create DP11iterative.cpp

---
+++ StriverDP/DP11iterative.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h> 
+int minimumPathSum(vector<vector<int>>& triangle, int n){
+	// Write your code here.
+
+	vector<vector<int>> dp(n, vector<int> (n, 0));
+	for(int i=0; i < n; i++) {
+		dp[n-1][i] = triangle[n-1][i];
+	}
+	for(int i=n-2; i>=0; i--) {
+		for(int j=i; j >= 0; j--) {
+			dp[i][j] = min(dp[i + 1][j], dp[i + 1][j + 1]) + triangle[i][j];
+		}
+	}
+
+	return dp[0][0];
+}
