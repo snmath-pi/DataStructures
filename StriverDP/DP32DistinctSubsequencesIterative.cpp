953b792032df97bf3d4792a629021790cb5aab1c
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2023-07-01 13:32:00 +0530

    Create DP32DistinctSubsequencesIterative.cpp

---
+++ StriverDP/DP32DistinctSubsequencesIterative.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        vector<vector<double>> dp(s.size() + 1, vector<double> (t.size() + 1));
+        for(int i=0; i<=s.size(); i++) dp[i][0] = 1;
+        for(int i=1; i<= s.size(); i++) {
+            for(int j=1; j<=t.size(); j++) {
+                if(s[i-1] == t[j-1]) dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                else dp[i][j] = dp[i-1][j];
+            }
+        }
+
+        return dp[s.size()][t.size()];
+    }
+};
