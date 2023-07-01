89a9e3420335621f3440147b408a2a6a8457d117
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2023-07-01 13:46:04 +0530

    Create DP32k1D.cpp

---
+++ StriverDP/DP32k1D.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int n = s.size(), m = t.length();
+        vector<double> dp(m + 1, 0);
+        dp[0] = 1;
+        for(int i=1; i<=n; i++) {
+            for(int j=m; j>=1; j--) {
+                if(s[i-1] == t[j-1]) dp[j] += dp[j-1];
+            }
+        }
+        return dp[m];
+    }
+};
