580465be3b65b321efeda6ebbcf1aef7f39a6a90
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2023-07-01 12:06:58 +0530

    Create DP27LongestCommonSubstring.cpp

---
+++ StriverDP/DP27LongestCommonSubstring.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h> 
+int lcs(string &str1, string &str2){
+	//	Write your code here.
+	int n = str1.length(), m = str2.length();
+	vector<vector<int>> dp(n + 1, vector<int> (m + 1, 0));
+	int ans = 0;
+	for(int i=1; i<=n; i++) {
+		for(int j=1; j<=m; j++) {
+			if(str1[i-1] == str2[j-1]) {
+				dp[i][j] = 1 + dp[i-1][j-1];
+				ans = max(ans, dp[i][j]);
+			} else dp[i][j] = 0;
+		}
+	}
+	return ans;
+}
