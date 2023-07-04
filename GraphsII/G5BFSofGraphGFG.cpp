f32cfdd631f6eab51714ebaac1c4d86ccfcb10b2
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2023-07-04 13:51:45 +0530

    Create G5BFSofGraphGFG.cpp

---
+++ GraphsII/G5BFSofGraphGFG.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to return Breadth First Traversal of given graph.
+    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        // Code here
+        vector<int> res;
+        queue<int> q; vector<bool> seen(V, 0);
+        seen[0] = 1;
+        q.push(0);
+        while(!q.empty()) {
+            int x = q.front(); q.pop();
+            res.push_back(x);
+            for(auto &y: adj[x]) {
+                if(!seen[y]) {
+                    seen[y] = 1;
+                    q.push(y);
+                }
+            }
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >>
+
+            E;
+
+        vector<int> adj[V];
+
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            // 		adj[v].push_back(u);
+        }
+        // string s1;
+        // cin>>s1;
+        Solution obj;
+        vector<int> ans = obj.bfsOfGraph(V, adj);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
