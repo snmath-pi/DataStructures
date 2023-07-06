c7c37b158abbe318da4f29942d7ec6aa5b6dde00
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2023-07-06 19:14:44 +0530

    Create R13(PermutationAllWay2).cpp

---
+++ Recursion/R13(PermutationAllWay2).cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void dfs(int id, vector<int> &nums, vector<vector<int>> &res) {
+        if(id == nums.size()) {
+            res.push_back(nums);
+            return;
+        }
+        for(int i =id; i<nums.size(); i++) {
+            swap(nums[i], nums[id]);
+            dfs(id+1, nums,res);
+            swap(nums[i], nums[id]);
+
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> res;
+        dfs(0, nums, res);
+        return res;
+    }
+};
