880dc0825c1fcec9284f749ecb6d139cea1ce34d
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2023-07-06 09:36:31 +0530

    Create SubsetSums(UsingBitManipulation).cpp

---
+++ StriverSheet/Recursion(Day 9)/SubsetSums(UsingBitManipulation).cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    vector<int> subsetSums(vector<int> arr, int N)
+    {
+        // Write Your Code here
+        vector<int> res;
+        for(int i=0; i<(1<<N); i++) {
+            int tot = 0;
+            for(int j=0; j<N; j++) {
+                if((i>>j) & 1) tot+=arr[j];
+            }
+            res.push_back(tot);
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin>>N;
+        vector<int> arr(N);
+        for(int i = 0 ; i < N ; i++){
+            cin >> arr[i];
+        }
+        Solution ob;
+        vector<int> ans = ob.subsetSums(arr,N);
+        sort(ans.begin(),ans.end());
+        for(auto sum : ans){
+            cout<< sum<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
