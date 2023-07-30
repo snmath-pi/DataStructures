e4bb0c8c3a1f2cb2af7c6be81f897430283fc3d9
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2023-07-30 22:53:26 +0530

    Update CountSteppingNumbersinRange.cpp

--- DigitDp/CountSteppingNumbersinRange.cpp
+++ DigitDp/CountSteppingNumbersinRange.cpp
@@ -1,18 +1,31 @@
 class Solution {
 public:
     #define ll long long
-    ll dp[110][2][2];
+    ll dp[110][2][2][12];
     const ll mod = 1e9 + 7;
-    long long dfs(string s, ll id, ll flag = 0, ll tight = 1) {
-        if(id == s.size()) return flag;
-        if(dp[id][flag][tight] != -1) return dp[id][flag][tight];
-        if(tight) {
-            
-        }
+    ll dfs(string &s, ll id = 0, ll flag = 1, ll tight = 1, ll prev = 0) {
+        if(id == (ll)s.size()) return !flag;
+        if(dp[id][flag][tight][prev] != -1) return dp[id][flag][tight][prev];
         ll res = 0;
-        for(int i=0; i<=9; i++) {
-            res += dfs()
+        ll lim = (tight == 1 ? (int)(s[id]-'0') : 9);
+        for(int i=0; i<=lim; i++) {
+            ll t = tight;
+            if(tight && i < lim) t = 0;
+            ll f = flag;
+            if(flag && i > 0) f = 0;
+
+            if(flag) {
+                res += dfs(s, id+1,f,t,i); res %= mod;
+            } else {
+                if(abs(i-prev) == 1) {
+                    res += dfs(s, id+1,f,t,i);
+                    res %=mod;
+                }
+            }
         }
+        // cout << prev << '\n';
+        // cout << res << '\n';
+        return dp[id][flag][tight][prev] = res % mod;
     }
     int countSteppingNumbers(string low, string high) {
         reverse(low.begin(), low.end());
@@ -28,12 +41,13 @@ public:
             left += (x + '0');
         }
         reverse(left.begin(), left.end());
-        cout << left << ' ';
+        // cout << left << ' ';
         memset(dp, -1, sizeof(dp));
-        ll u = dfs(left, 0) % mod;
+        ll u = dfs(left) % mod;
+        // cout << high << ' ';
         memset(dp, -1, sizeof(dp));
-        ll v = dfs(high, 0) % mod;
-        
+        ll v = dfs(high) % mod;
+        // cout << v << ' ';
         return (v-u+mod)%mod;
     }
 };
