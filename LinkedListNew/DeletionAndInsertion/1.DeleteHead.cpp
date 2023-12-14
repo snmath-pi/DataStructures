ee05da2af7e3a8fef038dfcce56228399d11e171
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2023-12-14 17:06:39 +0530

    Create 1.DeleteHead.cpp

---
+++ LinkedListNew/DeletionAndInsertion/1.DeleteHead.cpp
@@ -0,0 +1,31 @@
+/*
+Following is the class structure of the Node class:
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node()
+    {
+        this->data = 0;
+        next = NULL;
+    }
+    Node(int data)
+    {
+        this->data = data; 
+        this->next = NULL;
+    }
+    Node(int data, Node* next)
+    {
+        this->data = data;
+        this->next = next;
+    }
+};
+*/
+
+Node * deleteHead(Node *head) {
+    // Write your code here.
+    head = head->next;
+    return head;
+}
